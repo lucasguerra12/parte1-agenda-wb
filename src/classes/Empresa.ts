45566f196d86ef93ab155f3ec49940222ac657da
Lucas Guerra <lucasguerrawr2004@gmail.com>
2025-05-13 10:37:10 -0300

    Implement Empresa class with methods to manage clients, products, and services

---
+++ src/classes/Empresa.ts
@@ -0,0 +1,27 @@
+import { Cliente } from '../models/Cliente';
+import { Produto } from '../models/Produto';
+import { Servico } from '../models/Servico';
+
+export class Empresa {
+    private clientes: Array<Cliente>;
+    private produtos: Array<Produto>;
+    private servicos: Array<Servico>;
+
+    constructor() {
+        this.clientes = [];
+        this.produtos = [];
+        this.servicos = [];
+    }
+
+    public getClientes(): Array<Cliente> {
+        return this.clientes;
+    }
+
+    public getProdutos(): Array<Produto> {
+        return this.produtos;
+    }
+
+    public getServicos(): Array<Servico> {
+        return this.servicos;
+    }
+}
